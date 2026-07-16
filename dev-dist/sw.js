11999eba90bdd5e94a29dcfc899afba81e937658
aslam <aslam@smarthatch.com>
2026-07-16 19:51:34 +0530

    rounded added

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-afac4cd2'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.lokk951khbg"
+    "revision": "0.hsvhqbdibko"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
